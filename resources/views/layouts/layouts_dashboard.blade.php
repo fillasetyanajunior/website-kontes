5faf9260645807d428b3270e7890ec4f517ef3a2
fillajunior <pantailovina@gmail.com>
2021-11-07 16:28:22 +0800

    update 07/11/21

--- resources/views/layouts/layouts_dashboard.blade.php
+++ resources/views/layouts/layouts_dashboard.blade.php
@@ -132,7 +132,10 @@
             <div class="container">
                 <div class="row align-items-center flex-row-reverse">
                     <div class="col-12 col-lg-auto mt-3 mt-lg-0 text-center">
-                        Copyright © 2018 <a href=".">Tabler</a>. Theme by <a href="https://codecalm.net"
+                        Copyright © <script>
+                        document.write(new Date().getFullYear());
+
+                    </script> <a href=".">Tabler</a>. Theme by <a href="https://codecalm.net"
                             target="_blank">codecalm.net</a> All rights reserved.
                     </div>
                 </div>
